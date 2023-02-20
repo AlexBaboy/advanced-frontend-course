a77a993843315baeae431336617d3b79484e49c4
a.baboi <a.baboi.@pinkman.ru>
2023-02-20 23:58:21 +0200

    loaders

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,19 +3,24 @@ import {Route, Routes} from "react-router-dom";
 import {AboutPage} from "pages/About";
 import {MainPage} from "pages/Main";
 import {routeConfig} from "shared/config/routeConfig/routeConfig";
+import {PageLoader} from "widgets/PageLoader/PageLoader";
 
 export const AppRouter = () => {
     return (
-        <Suspense fallback={<div>Загрузка...</div>}>
+
             <Routes>
                 {Object.values(routeConfig).
                     map(({path, element}) => (
                         <Route
                             key={path}
                             path={path}
-                            element={(<div className={'page-wrapper'}>
+                            element={(
+                                <Suspense fallback={<PageLoader />}>
+                                    <div className={'page-wrapper'}>
                                         {element}
-                                    </div>)}
+                                    </div>
+                                </Suspense>
+                                )}
                         />
                     ))
                 }
@@ -24,6 +29,5 @@ export const AppRouter = () => {
                 <Route path={'/'}
                        element={<MainPage />}/>
             </Routes>
-        </Suspense>
     );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
+import {useTranslation} from "react-i18next";
 
 interface NavbarProps {
   className?: string
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+
+    const {t} = useTranslation()
+
   return (
       <div className={classNames(cls.Navbar, {}, [className])}>
 
@@ -16,11 +19,11 @@ export const Navbar = ({ className }: NavbarProps) => {
               <AppLink
                     theme={AppLinkTheme.SECONDARY}
                     to={'/'}
-                >Main page</AppLink>
+                >{t('Главная')}</AppLink>
               <AppLink
                     theme={AppLinkTheme.SECONDARY}
                     to={'/about'}
-                >About page</AppLink>
+                >{t('О приложении')}</AppLink>
           </div>
       </div>
   )
