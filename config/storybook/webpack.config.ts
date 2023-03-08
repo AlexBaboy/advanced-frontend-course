956d3e148afebca0a73a97e3d2395f2bae6bf96a
a.baboi <a.baboi.@pinkman.ru>
2023-03-08 23:34:44 +0200

    storybook decorators

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import {BuildPaths} from "../build/types/config";
 import path from "path";
+import {buildCssLoaders} from "../build/loaders/buildCssLoaders";
 
 export default ({config}: {config: webpack.Configuration}) => {
 
@@ -8,11 +9,12 @@ export default ({config}: {config: webpack.Configuration}) => {
         build: '',
         html: '',
         entry: '',
-        src: path.resolve(__dirname, '../', '../', 'src')
+        src: path.resolve(__dirname, '..', '..', 'src')
     }
 
-    config.resolve?.modules.push(paths.src)
-    config.resolve?.extensions.push('.ts', '.tsx')
+    config.resolve?.modules?.push(paths.src)
+    config.resolve?.extensions?.push('.ts', '.tsx')
+    config.module?.rules?.push(buildCssLoaders(true))
 
     return config
 }
--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import {Button, ThemeButton} from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -19,23 +19,17 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-    primary: true,
-    label: 'Button',
+    children: 'Text',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-    label: 'Button',
+export const Clear = Template.bind({});
+Clear.args = {
+    children: 'Text',
+    theme: ThemeButton.CLEAR
 };
 
-export const Large = Template.bind({});
-Large.args = {
-    size: 'large',
-    label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-    size: 'small',
-    label: 'Button',
+export const Outline = Template.bind({});
+Outline.args = {
+    children: 'Text',
+    theme: ThemeButton.OUTLINE
 };
