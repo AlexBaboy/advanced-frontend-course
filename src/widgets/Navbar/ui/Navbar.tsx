f8b3f5958f2886e79ab0cd98214f257d147ebbed
alexandr_b <alexandr_b@polbox.tv>
2023-03-23 09:32:54 +0200

    upgrade sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -10,20 +10,10 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
 
-    const {t} = useTranslation()
-
   return (
       <div className={classNames(cls.Navbar, {}, [className])}>
-
           <div className={cls.links}>
-              <AppLink
-                    theme={AppLinkTheme.SECONDARY}
-                    to={'/'}
-                >{t('Главная')}</AppLink>
-              <AppLink
-                    theme={AppLinkTheme.SECONDARY}
-                    to={'/about'}
-                >{t('О приложении')}</AppLink>
+              /
           </div>
       </div>
   )
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import {classNames} from 'shared/lib/classNames/classNames'
-import cls from './Sidebar.module.scss'
-import {useState} from 'react'
+import React, {useState} from 'react'
 import {ThemeSwitcher} from 'widgets/ThemeSwitcher'
 import {LangSwitcher} from 'widgets/Sidebar'
 import {Button, ButtonSize, ButtonTheme} from "shared/ui/Button/Button";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {useTranslation} from "react-i18next";
+import {RoutePath} from "shared/config/routeConfig/routeConfig";
+import AboutIcon from 'shared/assets/icons/clarity-list.svg'
+import MainIcon from 'shared/assets/icons/home.svg'
+import cls from './Sidebar.module.scss'
 
 interface SidebarProps {
   className?: string
@@ -11,6 +16,8 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false)
+
+    const {t} = useTranslation()
 
   const onToggle = () => {
     setCollapsed(prev => !prev)
@@ -30,6 +37,34 @@ export const Sidebar = ({ className }: SidebarProps) => {
           >
               {collapsed ? '>' : '<'}
           </Button>
+
+          <div className={cls.items}>
+              <div>
+                  <AppLink
+                      theme={AppLinkTheme.SECONDARY}
+                      to={RoutePath.main}
+                      className={cls.item}
+                  >
+                      <MainIcon className={cls.icon} />
+                      <span className={cls.link}>
+                          {t('Главная')}
+                      </span>
+                  </AppLink>
+              </div>
+              <div>
+                  <AppLink
+                      theme={AppLinkTheme.SECONDARY}
+                      to={RoutePath.about}
+                      className={cls.item}
+                  >
+                      <AboutIcon className={cls.icon} />
+                      <span className={cls.link}>
+                          {t('О приложении')}
+                      </span>
+                  </AppLink>
+              </div>
+          </div>
+
           <div className={cls.switchers}>
               <ThemeSwitcher />
               <LangSwitcher
