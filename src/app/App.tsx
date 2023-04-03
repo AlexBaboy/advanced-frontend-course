b2801a85aaed82576e1e9d7f099aa81ef1706b79
a.baboi <a.baboi.@pinkman.ru>
2023-04-03 23:12:09 +0300

    modal add

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,30 @@
-import React, {Suspense, useEffect} from 'react'
+import React, {Suspense, useEffect, useState} from 'react'
 import './styles/index.scss'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from 'app/providers/router'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
+import {Modal} from "shared/ui/Modal/Modal";
 
 const App = () => {
   const { theme } = useTheme()
+
+    const [isOpen, setIsOpen] = useState(false)
 
   return (
       <div className={classNames('app', { hovered: true, selectable: false }, [theme, 'class-test'])}>
           <Suspense fallback={''}>
               <Navbar />
+
+              <button onClick={() => setIsOpen(true)}>
+                  toggle
+              </button>
+              <Modal
+                  isOpen={isOpen}
+                  onClose={() => setIsOpen(false)}
+              />
+
               <div className={'content-page'}>
                   <Sidebar />
                   <AppRouter />
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,46 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Modal.module.scss'
+import {ReactNode} from "react";
+
+interface ModalProps {
+    className?: string
+    children?: ReactNode
+    isOpen?: boolean
+    onClose?: () => void
+}
+
+export const Modal = (props: ModalProps) => {
+
+    const {
+        className,
+        children,
+        isOpen,
+        onClose
+    } = props
+
+    const closeHandler = () => {
+        onClose && onClose()
+    }
+
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen
+    }
+
+    return (
+        <div className={classNames(
+                    cls.Modal,
+                    mods,
+            [className]
+            )}>
+
+            <div className={cls.overlay}>
+                <div className={cls.content}
+                    onClick={closeHandler}
+                >
+                    {children}
+                </div>
+            </div>
+
+        </div>
+    );
+};
