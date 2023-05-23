5da8a8b9547e5439f8d55a405c6c4d3400c388e4
a.baboi <a.baboi.@pinkman.ru>
2023-05-23 23:14:21 +0300

    authorize

--- src/features/AuthByUserName/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 
 import {LoginForm} from './LoginForm';
+import {StoreDecorator} from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -19,3 +20,31 @@ const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {};
+Primary.decorators = [StoreDecorator({
+    loginForm: {
+        username: 'usernameTest',
+        password: '123',
+        isLoading: false
+    }
+})]
+
+export const WithError = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+WithError.args = {};
+WithError.decorators = [StoreDecorator({
+    loginForm: {
+        username: 'usernameTest',
+        password: '123',
+        error: 'error message',
+        isLoading: false
+    }
+})]
+
+export const Loading = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Loading.args = {};
+Loading.decorators = [StoreDecorator({
+    loginForm: {
+        isLoading: true
+    }
+})]
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,11 +2,10 @@ import React, {useState} from 'react'
 import {classNames} from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import {useTranslation} from "react-i18next";
-import {Modal} from "shared/ui/Modal/Modal";
 import {Button, ButtonTheme} from "shared/ui/Button/Button";
 import {LoginModal} from "features/AuthByUserName";
-import {useSelector} from "react-redux";
-import {getUserAuthData} from "entities/User";
+import {useDispatch, useSelector} from "react-redux";
+import {getUserAuthData, userActions} from "entities/User";
 
 interface NavbarProps {
   className?: string
@@ -17,6 +16,7 @@ export const Navbar = ({ className }: NavbarProps) => {
     const {t} = useTranslation()
     const [isAuthModal, setIsAuthModal] = useState(false)
     const authData = useSelector(getUserAuthData)
+    const dispatch = useDispatch()
 
     const onCloseModal = () => {
         setIsAuthModal(false)
@@ -24,6 +24,24 @@ export const Navbar = ({ className }: NavbarProps) => {
 
     const onShowModal = () => {
         setIsAuthModal(true)
+    }
+
+    const onLogout = () => {
+        dispatch(userActions.logout())
+    }
+
+    if (authData) {
+        return (
+            <div className={classNames(cls.Navbar, {}, [className])}>
+                <Button
+                    theme={ButtonTheme.CLEAR_INVERTED}
+                    className={cls.links}
+                    onClick={onLogout}
+                >
+                    {t('Выйти')}
+                </Button>
+            </div>
+        )
     }
 
     return (
