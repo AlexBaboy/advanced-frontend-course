8fcb2427395114e6ffdd4a8ff9362d41016f1870
alexandr_b <alexandr_b@polbox.tv>
2023-07-07 10:18:13 +0300

    profile page

---
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type SidebarItemProps = {
+    className?: string
+}
+
+export const SidebarItem = ({className}: SidebarItemProps) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SidebarItem;
