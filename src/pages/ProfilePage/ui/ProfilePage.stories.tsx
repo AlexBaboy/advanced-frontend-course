a170242527f34fbf65d8746180674009c01a3b89
a.baboi <a.baboi.@pinkman.ru>
2023-07-13 23:42:31 +0300

    store decorator edited

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,6 +2,7 @@ import {ComponentMeta, ComponentStory} from '@storybook/react';
 import ProfilePage from './ProfilePage';
 import {ThemeDecorator} from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import {Theme} from "app/providers/ThemeProvider";
+import {StoreDecorator} from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -20,7 +21,14 @@ const Template: ComponentStory<typeof MainPage> = (args) => <ProfilePage {...arg
 export const Light = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Light.args = {};
+Light.decorators = [
+    ThemeDecorator(Theme.LIGHT),
+    StoreDecorator({})
+]
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)]
+Dark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({})
+]
