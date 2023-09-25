1e50b8f9edc8adce995c5c072c25049266b00134
Admin <alexbaboy2016@gmain.com>
2023-09-25 23:14:51 +0300

    form finished

---
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -0,0 +1,13 @@
+import {useSelector} from "react-redux";
+import {getUserAuthData} from "entities/User";
+import {useLocation, Navigate} from "react-router-dom";
+import {RoutePath} from "shared/config/routeConfig/routeConfig";
+
+export function RequireAuth({children}: {children: JSX.Element}) {
+    let auth = useSelector(getUserAuthData)
+    let location = useLocation()
+
+    if (!auth) {
+        return <Navigate to={RoutePath.main} state={{ from: location }} replace />
+    }
+}
