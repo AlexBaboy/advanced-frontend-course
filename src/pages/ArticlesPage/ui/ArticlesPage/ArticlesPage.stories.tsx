2c27d9f61bc01afb7188b51f09775ff50535e2df
alexandr_b <alexandr_b@polbox.tv>
2023-09-28 09:32:33 +0300

    articles stories

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -0,0 +1,25 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {ThemeDecorator} from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {Theme} from "app/providers/ThemeProvider";
+import {ArticlesPage} from "pages/ArticlesPage";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'pages/ArticlesPage',
+    component: ArticlesPage,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ArticlesPage>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ArticlesPage> = () => <ArticlesPage />;
+
+export const Light = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Light.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)]
