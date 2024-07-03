8b036df4f148846c344bdb1539225e36e4ac0f92
Admin <alexbaboy2016@gmain.com>
2024-07-03 08:11:20 +0300

    fix build project

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,44 +1,41 @@
-import webpack, {DefinePlugin, RuleSetRule} from 'webpack';
-import {BuildPaths} from "../build/types/config";
-import path from "path";
-import {buildCssLoaders} from "../build/loaders/buildCssLoaders";
+import webpack, { DefinePlugin, RuleSetRule } from 'webpack';
+import path from 'path';
+import {buildCssLoaders} from '../build/loaders/buildCssLoaders';
+import { BuildPaths } from '../build/types/config';
 
-export default ({config}: {config: webpack.Configuration}) => {
-
+export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
         build: '',
         html: '',
-        entry: './src/js/main.js',
+        entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
         locales: '',
         buildLocales: '',
-    }
+    };
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.extensions!.push('.ts', '.tsx');
 
-    config!.resolve!.modules = [paths.src, 'node_modules']
-    config!.resolve!.extensions!.push('.ts', '.tsx')
-
+    // eslint-disable-next-line no-param-reassign
     // @ts-ignore
-    config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
+    config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
-            return {
-                ...rule,
-                exclude: /\.svg$/i
-            }
+            return { ...rule, exclude: /\.svg$/i };
         }
-        return rule
-    })
+
+        return rule;
+    });
 
     config!.module!.rules.push({
         test: /\.svg$/,
-        use: ['@svgr/webpack']
-    })
-    config.module?.rules?.push(buildCssLoaders(true))
+        use: ['@svgr/webpack'],
+    });
+    config!.module!.rules.push(buildCssLoaders(true));
 
-    config.plugins?.push(new DefinePlugin({
-        __IS_DEV__: true,
-        __API__: JSON.stringify('http://testapi.ru'),
-        __PROJECT__: JSON.stringify('storybook')
-    }))
+    config!.plugins!.push(new DefinePlugin({
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify('https://testapi.ru'),
+        __PROJECT__: JSON.stringify('storybook'),
+    }));
 
-    return config
-}
+    return config;
+};
