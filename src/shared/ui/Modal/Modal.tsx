a69ec74520466d3c9e70f7a620069a4a77922d96
Admin <alexbaboy2016@gmain.com>
2024-07-10 21:22:13 +0300

    react-device-detect

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,7 +1,8 @@
 import {classNames, Mods} from "shared/lib/classNames/classNames";
 import cls from './Modal.module.scss'
 import React, {MutableRefObject, ReactNode, useEffect, useRef, useState} from "react";
-import {Portal} from "shared/ui/Portal/Portal";
+import {Portal} from "../Portal/Portal";
+import {Overlay} from "../Overlay/Overlay";
 
 interface ModalProps {
     className?: string
@@ -78,14 +79,9 @@ export const Modal = (props: ModalProps) => {
                 [className]
             )}>
 
-                <div className={cls.overlay}
-                     onClick={closeHandler}
-                >
-                    <div className={cls.content}
-                         onClick={onContentClick}
-                    >
-                        {children}
-                    </div>
+                <Overlay onClick={closeHandler} />
+                <div className={cls.content}>
+                    {children}
                 </div>
             </div>
         </Portal>
