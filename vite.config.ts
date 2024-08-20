f21a7bd553cd02734a177485f7a886f8005641e2
alexandr_b <alexandr_b@polbox.tv>
2024-08-20 09:47:04 +0300

    research svg errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
+// @ts-ignore
 export default defineConfig({
     plugins: [
-        react()
+        svgr({
+            svgrOptions: {
+                exportType: 'default',
+            },
+        }),
+        react(),
     ],
     resolve: {
         alias: [
-            {
-                find: '@', replacement: '/src'
-            }
-        ]
-    }
-})
+            { find: '@', replacement: '/src' },
+        ],
+    },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify('http://localhost:8000'),
+        __PROJECT__: JSON.stringify('frontend'),
+    },
+});
