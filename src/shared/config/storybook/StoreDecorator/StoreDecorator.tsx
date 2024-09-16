851eb36ef0864b535ab5bcb35257b423fa8d62b6
alexandr_b <alexandr_b@polbox.tv>
2024-09-16 10:02:39 +0300

    refactor

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,12 +1,13 @@
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
 import { DeepPartial } from '@reduxjs/toolkit';
-import { loginReducer } from '@/features/AuthByUserName/model/slice/loginSlice';
+
 import { ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { articleDetailsReducer } from '@/entities/Article/model/slice/articleDetailsSlice';
 import { addCommentFormReducer } from '@/features/addCommentForm/model/slice/addCommentFormSlice';
 import { articleDetailsPageReducer } from '@/pages/ArticleDetailsPage/model/slices';
 import { profileReducer } from '@/features/editableProfileCard/model/slice/profileSlice';
+import {loginReducer} from "@/features/AuthByUserName/testing";
 
 const defaultAsyncReducers: ReducersList = {
     loginForm: loginReducer,
