047efd33860a3e437e268d58a9e50e65879cff03
alexandr_b <alexandr_b@polbox.tv>
2024-10-02 09:42:07 +0300

    refactor

--- src/features/ArticleViewSelector/ArticleViewSelector.stories.tsx
+++ src/features/ArticleViewSelector/ArticleViewSelector.stories.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { ArticleTypeTabs } from '../../ui/ArticleTypeTabs/ArticleTypeTabs';
+import { ArticleView } from '../../entities/Article';
+import { ArticleViewSelector } from './ArticleViewSelector';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'entities/Articles/ArticleTypeTabs',
-    component: ArticleTypeTabs,
+    title: 'entities/Articles/ArticleViewSelector',
+    component: ArticleViewSelector,
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-} as ComponentMeta<typeof ArticleTypeTabs>;
+} as ComponentMeta<typeof ArticleViewSelector>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof ArticleTypeTabs> = (args) => <ArticleTypeTabs {...args} />;
+const Template: ComponentStory<typeof ArticleViewSelector> = (args) => <ArticleViewSelector {...args} />;
 
 export const Big = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Big.args = {
+    view: ArticleView.BIG,
 };
 
 export const Small = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Small.args = {
+    view: ArticleView.SMALL,
 };
