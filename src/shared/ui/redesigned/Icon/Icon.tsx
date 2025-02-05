2d079076d40c42f3e1244553505935f625022a2b
alexandr_b <alexandr_b@polbox.tv>
2025-02-05 09:58:09 +0200

    refactor

--- src/shared/ui/redesigned/Icon/Icon.tsx
+++ src/shared/ui/redesigned/Icon/Icon.tsx
@@ -3,10 +3,23 @@ import React, { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Icon.module.scss';
 
-interface IconProps extends React.SVGProps<SVGSVGElement> {
+type SvgProps = Omit<React.SVGProps<SVGSVGElement>, 'onClick'>;
+
+interface IconBaseProps extends SvgProps {
     className?: string;
     Svg: React.VFC<React.SVGProps<SVGSVGElement>>;
 }
+
+interface NonClickableIconProps extends IconBaseProps {
+    clickable?: false;
+}
+
+interface ClickableBaseProps extends IconBaseProps {
+    clickable: true;
+    onCLick: () => void;
+}
+
+type IconProps = NonClickableIconProps | ClickableBaseProps;
 
 export const Icon = memo((props: IconProps) => {
     const { className, Svg, width = 32, height = 32, ...otherProps } = props;
