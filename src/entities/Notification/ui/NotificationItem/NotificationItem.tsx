83fedef778ed79bcbb9f0ef43e6752cc0626f8db
alexandr_b <alexandr_b@polbox.tv>
2025-02-12 08:53:58 +0200

    refactor

--- src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
@@ -4,8 +4,14 @@ import { classNames } from '@/shared/lib/classNames/classNames';
 import { useNotifications } from '@/entities/Notification/api/notificationApi';
 import cls from './NotificationItem.module.scss';
 import { Notification } from '../../model/types/notifications';
-import { Card, CardTheme } from '@/shared/ui/deprecated/Card/Card';
-import { Text } from '@/shared/ui/deprecated/Text/Text';
+import {
+    Card as CardDeprecated,
+    CardTheme,
+} from '@/shared/ui/deprecated/Card/Card';
+import { Text as TextDeprecated } from '@/shared/ui/deprecated/Text/Text';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Card } from '@/shared/ui/redesigned/Card/Card';
+import { Text } from '@/shared/ui/redesigned/Text/Text';
 
 interface NotificationItemProps {
     className?: string;
@@ -15,16 +21,32 @@ interface NotificationItemProps {
 export const NotificationItem = memo((props: NotificationItemProps) => {
     const { className, item } = props;
 
-    const { t } = useTranslation();
-    const { data, isLoading } = useNotifications(null);
-
     const content = (
-        <Card
-            theme={CardTheme.OUTLINED}
-            className={classNames(cls.NotificationItem, {}, [className])}
-        >
-            <Text title={item.title} text={item.description} />
-        </Card>
+        <ToggleFeatures
+            on={
+                <Card
+                    className={classNames(cls.NotificationItem, {}, [
+                        className,
+                    ])}
+                >
+                    <Text title={item.title} text={item.description} />
+                </Card>
+            }
+            off={
+                <CardDeprecated
+                    theme={CardTheme.OUTLINED}
+                    className={classNames(cls.NotificationItem, {}, [
+                        className,
+                    ])}
+                >
+                    <TextDeprecated
+                        title={item.title}
+                        text={item.description}
+                    />
+                </CardDeprecated>
+            }
+            feature={'isAppRedesigned'}
+        />
     );
 
     if (item.href) {
