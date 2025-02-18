7d618c0098ca84b205a8b21d31fa6fa6bd7d7cb6
alexandr_b <alexandr_b@polbox.tv>
2025-02-18 09:50:58 +0200

    article list

--- src/entities/Notification/ui/NotificationList/NotificationList.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.tsx
@@ -6,7 +6,9 @@ import { useNotifications } from '@/entities/Notification/api/notificationApi';
 import cls from './NotificationList.module.scss';
 import { VStack } from '@/shared/ui/redesigned/Stack';
 import { NotificationItem } from '../NotificationItem/NotificationItem';
-import { Skeleton } from '@/shared/ui/deprecated/Skeleton/Skeleton';
+import { Skeleton as SkeletonDeprecated } from '@/shared/ui/deprecated/Skeleton/Skeleton';
+import { Skeleton as SkeletonRedesigned } from '@/shared/ui/redesigned/Skeleton/Skeleton';
+import { toggleFeatures } from '@/shared/lib/features';
 
 interface NotificationListProps {
     className?: string;
@@ -20,6 +22,12 @@ export const NotificationList = memo((props: NotificationListProps) => {
     const { t } = useTranslation();
     const { data, isLoading } = useNotifications(null, {
         pollingInterval: 5000,
+    });
+
+    const Skeleton = toggleFeatures({
+        name: 'isAppRedesigned',
+        on: () => SkeletonRedesigned,
+        off: () => SkeletonDeprecated,
     });
 
     if (isLoading) {
