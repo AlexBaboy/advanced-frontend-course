9f579b23355b73942fe5c3dc5724419c831a05bb
Admin <alexbaboy2016@gmain.com>
2025-02-25 20:31:22 +0200

    login form

--- src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
@@ -1,24 +1,29 @@
-import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
-import { useSelector } from 'react-redux';
-import { AnyAction } from '@reduxjs/toolkit';
-import { classNames } from '@/shared/lib/classNames/classNames';
+import {useTranslation} from 'react-i18next';
+import {memo} from 'react';
+import {useSelector} from 'react-redux';
+import {AnyAction} from '@reduxjs/toolkit';
+import {classNames} from '@/shared/lib/classNames/classNames';
 import cls from './LoginForm.module.scss';
-import { Button, ButtonTheme } from '@/shared/ui/deprecated/Button/Button';
-import { Input } from '@/shared/ui/deprecated/Input/Input';
-import { loginActions, loginReducer } from '../../model/slice/loginSlice';
+import {Button as ButtonDeprecated, ButtonTheme} from '@/shared/ui/deprecated/Button/Button';
+import {Button} from '@/shared/ui/redesigned/Button/Button';
+import {Input as InputDeprecated} from '@/shared/ui/deprecated/Input/Input';
+import {Input} from '@/shared/ui/redesigned/Input/Input';
+import {loginActions, loginReducer} from '../../model/slice/loginSlice';
 
-import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
-import { TextTheme, Text } from '@/shared/ui/deprecated/Text/Text';
-import { getLoginUsername } from '../../model/selectors/getLoginUsername/getLoginUsername';
-import { getLoginIsLoading } from '../../model/selectors/getLoginIsLoading/getLoginIsLoading';
-import { getLoginError } from '../../model/selectors/getLoginError/getLoginError';
-import { getLoginPassword } from '../../model/selectors/getLoginPassword/getLoginPassword';
+import {loginByUsername} from '../../model/services/loginByUsername/loginByUsername';
+import {TextTheme, Text as TextDeprecated} from '@/shared/ui/deprecated/Text/Text';
+import {Text} from '@/shared/ui/redesigned/Text/Text';
+import {getLoginUsername} from '../../model/selectors/getLoginUsername/getLoginUsername';
+import {getLoginIsLoading} from '../../model/selectors/getLoginIsLoading/getLoginIsLoading';
+import {getLoginError} from '../../model/selectors/getLoginError/getLoginError';
+import {getLoginPassword} from '../../model/selectors/getLoginPassword/getLoginPassword';
 import {
     DynamicModuleLoader,
     ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import {useAppDispatch} from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import {ToggleFeatures} from "@/shared/lib/features";
+import {VStack} from "@/shared/ui/redesigned/Stack";
 
 export interface LoginFormProps {
     className?: string;
@@ -29,8 +34,8 @@ const initialReducers: ReducersList = {
     loginForm: loginReducer,
 };
 
-const LoginForm = memo(({ className, onSuccess }: LoginFormProps) => {
-    const { t } = useTranslation();
+const LoginForm = memo(({className, onSuccess}: LoginFormProps) => {
+    const {t} = useTranslation();
     const dispatch = useAppDispatch();
 
     const username = useSelector(getLoginUsername);
@@ -61,33 +66,67 @@ const LoginForm = memo(({ className, onSuccess }: LoginFormProps) => {
 
     return (
         <DynamicModuleLoader removeAfterUnmount reducers={initialReducers}>
-            <div className={classNames(cls.LoginForm, {}, [className])}>
-                <Text title={t('Форма авторизации')} />
-                {error && <Text text={error} theme={TextTheme.ERROR} />}
-                <Input
-                    placeholder={t('Введите логин')}
-                    type="text"
-                    className={cls.input}
-                    autofocus
-                    onChange={onChangeUserName}
-                    value={username}
-                />
-                <Input
-                    placeholder={t('Введите пароль')}
-                    type="text"
-                    className={cls.input}
-                    onChange={onChangePassword}
-                    value={password}
-                />
-                <Button
-                    theme={ButtonTheme.OUTLINE}
-                    className={cls.loginBtn}
-                    onClick={onLoginClick}
-                    disabled={isLoading}
-                >
-                    {t('Войти')}
-                </Button>
-            </div>
+
+            <ToggleFeatures
+                feature={'isAppRedesigned'}
+                on={
+                <VStack gap={'8'} className={classNames(cls.LoginForm, {}, [className])}>
+                    <Text title={t('Форма авторизации')} />
+                    {error && <Text text={error} variant={'error'} />}
+                    <Input
+                        placeholder={t('Введите логин')}
+                        type="text"
+                        className={cls.input}
+                        autofocus
+                        onChange={onChangeUserName}
+                        value={username}
+                    />
+                    <Input
+                        placeholder={t('Введите пароль')}
+                        type="text"
+                        className={cls.input}
+                        onChange={onChangePassword}
+                        value={password}
+                    />
+                    <Button
+                        variant={'outline'}
+                        className={cls.loginBtn}
+                        onClick={onLoginClick}
+                        disabled={isLoading}
+                    >
+                        {t('Войти')}
+                    </Button>
+                </VStack>}
+                off={<div className={classNames(cls.LoginForm, {}, [className])}>
+                    <TextDeprecated title={t('Форма авторизации')}/>
+                    {error && <TextDeprecated text={error} theme={TextTheme.ERROR}/>}
+                    <InputDeprecated
+                        placeholder={t('Введите логин')}
+                        type="text"
+                        className={cls.input}
+                        autofocus
+                        onChange={onChangeUserName}
+                        value={username}
+                    />
+                    <InputDeprecated
+                        placeholder={t('Введите пароль')}
+                        type="text"
+                        className={cls.input}
+                        onChange={onChangePassword}
+                        value={password}
+                    />
+                    <ButtonDeprecated
+                        theme={ButtonTheme.OUTLINE}
+                        className={cls.loginBtn}
+                        onClick={onLoginClick}
+                        disabled={isLoading}
+                    >
+                        {t('Войти')}
+                    </ButtonDeprecated>
+                </div>}
+            />
+
+
         </DynamicModuleLoader>
     );
 });
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,25 +1,26 @@
-import React, { memo, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
-import { classNames } from '@/shared/lib/classNames/classNames';
-import { Button, ButtonTheme } from '@/shared/ui/deprecated/Button/Button';
-import { LoginModal } from '@/features/AuthByUserName';
-import { getUserAuthData } from '@/entities/User';
-import { Text, TextTheme } from '@/shared/ui/deprecated/Text/Text';
-import { AppLink } from '@/shared/ui/redesigned/AppLink/AppLink';
-import { getRouteArticleCreate } from '@/shared/config/routeConfig/routeConfig';
+import React, {memo, useState} from 'react';
+import {useTranslation} from 'react-i18next';
+import {useSelector} from 'react-redux';
+import {classNames} from '@/shared/lib/classNames/classNames';
+import {Button as ButtonDeprecated, ButtonTheme} from '@/shared/ui/deprecated/Button/Button';
+import {Button} from '@/shared/ui/redesigned/Button/Button';
+import {LoginModal} from '@/features/AuthByUserName';
+import {getUserAuthData} from '@/entities/User';
+import {Text, TextTheme} from '@/shared/ui/deprecated/Text/Text';
+import {AppLink} from '@/shared/ui/redesigned/AppLink/AppLink';
+import {getRouteArticleCreate} from '@/shared/config/routeConfig/routeConfig';
 import cls from './Navbar.module.scss';
-import { HStack } from '@/shared/ui/redesigned/Stack';
-import { NotificationButton } from '@/features/notificationButton';
-import { AvatarDropdown } from '@/features/avatarDropdown';
-import { ToggleFeatures } from '@/shared/lib/features';
+import {HStack} from '@/shared/ui/redesigned/Stack';
+import {NotificationButton} from '@/features/notificationButton';
+import {AvatarDropdown} from '@/features/avatarDropdown';
+import {toggleFeatures, ToggleFeatures} from '@/shared/lib/features';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = memo(({ className }: NavbarProps) => {
-    const { t } = useTranslation();
+export const Navbar = memo(({className}: NavbarProps) => {
+    const {t} = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
     const authData = useSelector(getUserAuthData);
 
@@ -31,23 +32,30 @@ export const Navbar = memo(({ className }: NavbarProps) => {
         setIsAuthModal(true);
     };
 
+
+    const mainClass = toggleFeatures({
+        name: 'isAppRedesigned',
+        on: () => cls.NavbarRedesigned,
+        off: () => cls.Navbar,
+    })
+
     const renderComponent = () => {
         return (
             <ToggleFeatures
                 on={
                     <header
-                        className={classNames(cls.NavbarRedesigned, {}, [
+                        className={classNames(mainClass, {}, [
                             className,
                         ])}
                     >
                         <HStack gap="16" className={cls.actions}>
-                            <NotificationButton />
-                            <AvatarDropdown />
+                            <NotificationButton/>
+                            <AvatarDropdown/>
                         </HStack>
                     </header>
                 }
                 off={
-                    <header className={classNames(cls.Navbar, {}, [className])}>
+                    <header className={classNames(mainClass, {}, [className])}>
                         <div className={cls.headerLeftPart}>
                             <Text
                                 title="Art App"
@@ -64,8 +72,8 @@ export const Navbar = memo(({ className }: NavbarProps) => {
                         </div>
 
                         <HStack gap="16" className={cls.actions}>
-                            <NotificationButton />
-                            <AvatarDropdown />
+                            <NotificationButton/>
+                            <AvatarDropdown/>
                         </HStack>
                     </header>
                 }
@@ -79,17 +87,28 @@ export const Navbar = memo(({ className }: NavbarProps) => {
     }
 
     return (
-        <header className={classNames(cls.Navbar, {}, [className])}>
-            <Button
-                theme={ButtonTheme.CLEAR_INVERTED}
-                className={cls.links}
-                onClick={onShowModal}
-            >
-                {t('Войти')}
-            </Button>
+        <header className={classNames(mainClass, {}, [className])}>
+
+            <ToggleFeatures
+                feature={'isAppRedesigned'}
+                on={<Button
+                    variant={'clear'}
+                    className={cls.links}
+                    onClick={onShowModal}
+                >
+                    {t('Войти')}
+                </Button>}
+                off={<ButtonDeprecated
+                    theme={ButtonTheme.CLEAR_INVERTED}
+                    className={cls.links}
+                    onClick={onShowModal}
+                >
+                    {t('Войти')}
+                </ButtonDeprecated>}
+            />
 
             {isAuthModal && (
-                <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
+                <LoginModal isOpen={isAuthModal} onClose={onCloseModal}/>
             )}
         </header>
     );
