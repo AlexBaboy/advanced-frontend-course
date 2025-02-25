ef53b430cf8dbfec96b0654ce323e8ba42af05db
alexandr_b <alexandr_b@polbox.tv>
2025-02-25 09:30:08 +0200

    article rating

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
@@ -1,11 +1,16 @@
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text, TextSize } from '@/shared/ui/deprecated/Text/Text';
+import {
+    Text as TextDeprecated,
+    TextSize,
+} from '@/shared/ui/deprecated/Text/Text';
+import { Text } from '@/shared/ui/redesigned/Text/Text';
 import { ArticleList } from '@/entities/Article';
 import { VStack } from '@/shared/ui/redesigned/Stack';
 import { LIMIT_RECOMMENDATIONS_COUNT } from '@/pages/ArticleDetailsPage/model/services/fetchArticleRecommendations/fetchArticleRecommendations';
 import { useArticlesRecommendationsList } from '@/features/articleRecommendationsList/ui/api/articleRecommendationsApi';
+import { ToggleFeatures } from '@/shared/lib/features';
 
 interface ArticleRecommendationsListProps {
     className?: string;
@@ -30,7 +35,17 @@ export const ArticleRecommendationsList = memo(
                 gap="8"
                 className={classNames('', {}, [className])}
             >
-                <Text title={t('Рекомендуем')} size={TextSize.L} />
+                <ToggleFeatures
+                    feature={'isAppRedesigned'}
+                    on={<Text title={t('Рекомендуем')} size={'l'} />}
+                    off={
+                        <TextDeprecated
+                            title={t('Рекомендуем')}
+                            size={TextSize.L}
+                        />
+                    }
+                />
+
                 <ArticleList
                     articles={articles}
                     isLoading={isLoading}
